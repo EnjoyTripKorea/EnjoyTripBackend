168fa28f5bde1f0fbeafc7572c2d391f752c626c
서지현 <sgh9702@naver.com>
2024-05-07 16:54:41 +0900

    feat : initial setting

---
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/EnjoyTripBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnjoyTripBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyTripBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyTripBackendApplication.class, args);
+	}
+
+}
---
+++ EnjoyTripBackend/src/test/java/com/example/EnjoyTripBackend/EnjoyTripBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnjoyTripBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoyTripBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
