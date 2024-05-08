12b8f6da88397d8149297620c7fb05f63d822d7d
서지현 <sgh9702@naver.com>
2024-05-08 11:13:36 +0900

    feat(#1) : 회원가입 시 유효성 검사 구현

--- EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/controller/MemberController.java
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/controller/MemberController.java
@@ -2,8 +2,10 @@ package com.example.EnjoyTripBackend.controller;
 
 import com.example.EnjoyTripBackend.dto.member.SignUpRequestDto;
 import com.example.EnjoyTripBackend.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signUp")
-    public ResponseEntity<?> signUp(@RequestBody SignUpRequestDto signUpRequestDto){
+    public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequestDto signUpRequestDto, BindingResult bindingResult){
         memberService.signUp(signUpRequestDto);
         return ResponseEntity.ok().body("회원가입 완료");
     }
--- EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/dto/member/SignUpRequestDto.java
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/dto/member/SignUpRequestDto.java
@@ -1,6 +1,10 @@
 package com.example.EnjoyTripBackend.dto.member;
 
 import com.example.EnjoyTripBackend.domain.Gender;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.NoArgsConstructor;
 import lombok.Getter;
 
@@ -8,11 +12,25 @@ import lombok.Getter;
 @NoArgsConstructor
 public class SignUpRequestDto {
 
+    @NotEmpty
+    @Email
     private String email;
+
+    @NotEmpty
     private String username;
+
+    @NotEmpty
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*\\p{Punct}).{2,20}$", message = "영문/숫자/특수문자 2~20자 이내로 작성해주세요")
     private String password;
+
+    @NotNull
     private Integer age;
+
+    @NotNull
     private Gender gender;
+
+    @NotEmpty
+    @Pattern(regexp = "^[0-9]{11}")
     private String phoneNumber;
 
 }
