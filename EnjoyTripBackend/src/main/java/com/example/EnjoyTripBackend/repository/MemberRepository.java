9d7554baf56ddbe39009a5133ba5066d39dd30b1
서지현 <sgh9702@naver.com>
2024-05-08 09:51:25 +0900

    feat(#1) : 이메일 중복 검사 구현

--- EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/repository/MemberRepository.java
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/repository/MemberRepository.java
@@ -4,7 +4,11 @@ package com.example.EnjoyTripBackend.repository;
 import com.example.EnjoyTripBackend.domain.Member;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Optional;
+
 @Mapper
 public interface MemberRepository {
     Long save(Member member);
+
+    Optional<Member> findByEmail(String email);
 }
--- EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/service/MemberService.java
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/service/MemberService.java
@@ -3,10 +3,14 @@ package com.example.EnjoyTripBackend.service;
 import com.example.EnjoyTripBackend.domain.Member;
 import com.example.EnjoyTripBackend.domain.MemberRole;
 import com.example.EnjoyTripBackend.dto.member.SignUpRequestDto;
+import com.example.EnjoyTripBackend.exception.EnjoyTripException;
+import com.example.EnjoyTripBackend.exception.ErrorCode;
 import com.example.EnjoyTripBackend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +21,11 @@ public class MemberService {
 
     @Transactional
     public Long signUp(SignUpRequestDto signUpRequestDto) {
+
+        Optional<Member> existMember = memberRepository.findByEmail(signUpRequestDto.getEmail());
+        if (existMember.isPresent()){
+            throw new EnjoyTripException(ErrorCode.DUPLICATE_EMAIL);
+        }
 
         Member member = Member.builder()
                 .email(signUpRequestDto.getEmail())
