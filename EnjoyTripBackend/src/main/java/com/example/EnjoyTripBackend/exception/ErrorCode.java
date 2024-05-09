f4addab58714afd5c4ec9f38ee5ee39e18ad56a4
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2024-05-10 00:34:09 +0900

    feat(#4) : 서블릿 필터 인증된 사용자 검증

--- EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/exception/ErrorCode.java
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일 계정입니다."),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
-    LOGIN_FAIL(HttpStatus.UNAUTHORIZED,"아이디 또는 비밀번호가 잘못 되었습니다.");
+    LOGIN_FAIL(HttpStatus.UNAUTHORIZED,"아이디 또는 비밀번호가 잘못 되었습니다."),
+    ANONYMOUS_USER(HttpStatus.UNAUTHORIZED,"인증되지 않는 사용자입니다. 로그인을 진행해 주세요");
 
     private final HttpStatus httpstatus;
     private final String message;
---
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/filter/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.EnjoyTripBackend.filter;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean LoginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+}
