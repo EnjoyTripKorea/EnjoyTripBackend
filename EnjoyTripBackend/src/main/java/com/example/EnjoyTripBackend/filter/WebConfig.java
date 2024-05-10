02610da6c312b43964df9f8827749db82b5bebbd
서지현 <sgh9702@naver.com>
2024-05-10 09:44:22 +0900

    feat(#4) : argument resolver 를 이용한 세션 저장소로 부터 데이터 바인딩 구현

--- EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/filter/WebConfig.java
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/filter/WebConfig.java
@@ -1,12 +1,22 @@
 package com.example.EnjoyTripBackend.filter;
 
+import com.example.EnjoyTripBackend.util.SessionUserArgumentResolver;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new SessionUserArgumentResolver());
+    }
 
     @Bean
     public FilterRegistrationBean LoginCheckFilter() {
