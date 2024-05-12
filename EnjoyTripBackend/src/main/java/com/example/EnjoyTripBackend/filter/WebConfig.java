9e8f68edec57a13aab1bf563f22f07e9bd5f757f
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2024-05-12 12:03:58 +0900

    feat(#6) : CORS filter 구현

--- EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/filter/WebConfig.java
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/filter/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.EnjoyTripBackend.filter;
 import com.example.EnjoyTripBackend.util.SessionUserArgumentResolver;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.Filter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -24,6 +26,15 @@ public class WebConfig implements WebMvcConfigurer {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter(new ObjectMapper()));
         filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean CorsFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new CorsFilter());
+        filterRegistrationBean.setOrder(0);
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
     }
