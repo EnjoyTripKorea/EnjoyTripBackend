4d0e80360dd0d4b96d2026a430cc2fa1c9baa6b8
송재원 <fosong98@gmail.com>
2024-05-14 16:32:51 +0900

    feat(#2) : 게시판 도메인 설계 개발

---
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/domain/Board.java
@@ -0,0 +1,19 @@
+package com.example.EnjoyTripBackend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board extends BaseTimeDomain{
+
+    private Long id;
+    private Long memberId;
+    private String title;
+    private String content;
+
+}
