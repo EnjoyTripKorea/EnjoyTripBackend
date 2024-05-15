0d694e01e351ced90f923d759913030e42cf2f9d
Jiwon <wldnjs980409@naver.com>
2024-05-15 18:21:50 +0900

    feat(#2) : BoardDto 어노테이션 추가

--- EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/dto/Board/BoardDto.java
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/dto/Board/BoardDto.java
@@ -2,11 +2,13 @@ package com.example.EnjoyTripBackend.dto.Board;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class BoardDto {
 
     @NotNull
