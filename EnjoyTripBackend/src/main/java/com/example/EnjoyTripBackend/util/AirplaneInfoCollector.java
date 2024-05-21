0d0a9f98b07be2164cbe769106658a160f4f23bb
DebbieIsFree <wldnjs980409@naver.com>
2024-05-21 17:11:19 +0900

    feat(#23) : 항공권(airplane) API 기능 구현

--- EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/util/AirplaneInfoCollector.java
+++ EnjoyTripBackend/src/main/java/com/example/EnjoyTripBackend/util/AirplaneInfoCollector.java
@@ -1,11 +1,10 @@
 package com.example.EnjoyTripBackend.util;
 
-import com.example.EnjoyTripBackend.domain.Airplane;
-import com.example.EnjoyTripBackend.dto.airplane.AirplaneResponseDto;
+import com.example.EnjoyTripBackend.dto.airplane.AirplaneDto;
+import com.example.EnjoyTripBackend.dto.airplane.AirplaneItem;
 import com.example.EnjoyTripBackend.exception.EnjoyTripException;
 import com.example.EnjoyTripBackend.exception.ErrorCode;
 import com.example.EnjoyTripBackend.service.AirplaneService;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,87 +21,49 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AirplaneInfoCollector {
 
-    @Value("${flight.service.key}")
+    @Value("${airplane.url}")
     private String serviceKey;
 
     private final AirplaneService airplaneService;
-    private final RestTemplate restTemplate;
-    private final ObjectMapper objectMapper;
 
-    @Scheduled(cron = "0 48 19 * * *") // 매일 오후 7시 48분에 실행
+    @Scheduled(cron = "* * * * * *")
     public void fetchFlightInfo() {
         LocalDate today = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
-        List<String> depAirportIds = List.of("NAARKJB", "NAARKJJ", "NAARKJK", "NAARKJY", "NAARKNW", "NAARKNY", "NAARKPC", "NAARKPK", "NAARKPS", "NAARKPU", "NAARKSI", "NAARKSS", "NAARKTH", "NAARKTN", "NAARKTU");
-        List<String> arrAirportIds = List.of("NAARKJB", "NAARKJJ", "NAARKJK", "NAARKJY", "NAARKNW", "NAARKNY", "NAARKPC", "NAARKPK", "NAARKPS", "NAARKPU", "NAARKSI", "NAARKSS", "NAARKTH", "NAARKTN", "NAARKTU");
+        List<String> airports = List.of("NAARKJB", "NAARKJJ", "NAARKJK", "NAARKJY", "NAARKNW", "NAARKNY", "NAARKPC", "NAARKPK", "NAARKPS", "NAARKPU", "NAARKSI", "NAARKSS", "NAARKTH", "NAARKTN", "NAARKTU");
         List<String> airlineIds = List.of("AAR", "ABL", "ASV", "ESR", "FGW", "HGG", "JJA", "JNA", "KAL", "TWB");
 
-        for (int i = 0; i < 31; i++) { // Collect data for 31 days
+        String url = "http://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?&_type=json&serviceKey=" + serviceKey;
+
+        for (int i = 0; i < 31; i++) {
             LocalDate date = today.plusDays(i);
-            String depPlandTime = date.format(formatter);
+            String formattedDate = date.format(formatter);
 
-            for (String depAirportId : depAirportIds) {
-                for (String arrAirportId : arrAirportIds) {
-                    if (!depAirportId.equals(arrAirportId)) { // Ensure departure and arrival airports are different
-                        for (String airlineId : airlineIds) {
-                            fetchAndSaveFlightsForDate(depAirportId, arrAirportId, depPlandTime, airlineId);
+            for (String depAirportId : airports) {
+                for (String arrAirportId : airports) {
+                    if (!depAirportId.equals(arrAirportId)) {
+                        final String targetUrl = url + "&depAirportId=" + depAirportId + "&arrAirportId=" + arrAirportId + "&depPlandTime=" + formattedDate;
+                        RestTemplate rt = new RestTemplate();
+                        ResponseEntity<String> response = rt.getForEntity(targetUrl, String.class);
+                        ObjectMapper objectMapper = new ObjectMapper();
+
+                        try {
+                            AirplaneDto responseObject = objectMapper.readValue(response.getBody(), AirplaneDto.class);
+
+                            int size = responseObject.getResponse().getBody().getItems().getItem().size();
+
+                            for (int n = 0; n < size; n++) {
+                                AirplaneItem item = responseObject.getResponse().getBody().getItems().getItem().get(n);
+                                airplaneService.save(item);
+                                System.out.println(item);
+                            }
+                        } catch (Exception e) {
+                            throw new EnjoyTripException(ErrorCode.FAIL_INSERT_AIRPLANE_DATA, e.getMessage());
                         }
                     }
                 }
             }
         }
-    }
-
-    private void fetchAndSaveFlightsForDate(String depAirportId, String arrAirportId, String depPlandTime, String airlineId) {
-        String url = String.format("http://apis.data.go.kr/1613000/DmstcFlightNvgInfoService/getFlightOpratInfoList?serviceKey=%s&_type=json&depAirportId=%s&arrAirportId=%s&depPlandTime=%s&airlineId=%s",
-                serviceKey, depAirportId, arrAirportId, depPlandTime, airlineId);
-
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
-
-        if (responseEntity.getStatusCode().is2xxSuccessful()) {
-            String responseBody = responseEntity.getBody();
-
-            try {
-                JsonNode rootNode = objectMapper.readTree(responseBody);
-                JsonNode itemsNode = rootNode.path("response").path("body").path("items").path("item");
-
-                if (itemsNode.isArray()) {
-                    for (JsonNode itemNode : itemsNode) {
-                        String airlineNm = getSafeText(itemNode, "airlineNm");
-                        String arrAirportNm = getSafeText(itemNode, "arrAirportNm");
-                        String depAirportNm = getSafeText(itemNode, "depAirportNm");
-                        String depPlandTimeStr = getSafeText(itemNode, "depPlandTime");
-                        String arrPlandTimeStr = getSafeText(itemNode, "arrPlandTime");
-                        String economyCharge = getSafeText(itemNode, "economyCharge");
-                        String prestigeCharge = getSafeText(itemNode, "prestigeCharge");
-                        String vihicleId = getSafeText(itemNode, "vihicleId");
-
-                        if (arrAirportNm != null && depAirportNm != null && !arrAirportNm.equals(depAirportNm)) {
-                            AirplaneResponseDto airplaneDto = new AirplaneResponseDto();
-                            airplaneDto.setAirlineNm(airlineNm);
-                            airplaneDto.setArrAirportNm(arrAirportNm);
-                            airplaneDto.setDepAirportNm(depAirportNm);
-                            airplaneDto.setDepPlandTime(depPlandTimeStr);
-                            airplaneDto.setArrPlandTime(arrPlandTimeStr);
-                            airplaneDto.setEconomyCharge(economyCharge);
-                            airplaneDto.setPrestigeCharge(prestigeCharge);
-                            airplaneDto.setVihicleId(vihicleId);
-
-                            airplaneService.save(airplaneDto);
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                throw new EnjoyTripException(ErrorCode.FAIL_INSERT_AIRPLANE_DATA, e.getMessage());
-            }
-        } else {
-            throw new EnjoyTripException(ErrorCode.FAIL_INSERT_AIRPLANE_DATA, "Failed to fetch airplane data from the API");
-        }
-    }
-
-    private String getSafeText(JsonNode node, String key) {
-        JsonNode valueNode = node.get(key);
-        return valueNode != null ? valueNode.asText() : null;
     }
 }
